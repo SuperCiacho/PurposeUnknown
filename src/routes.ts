5c78f71d6b45933274d04d8578b1c9f46fa5fb91
Bartosz <bartosznovak@gmail.com>
2019-12-23 19:19:47 +0100

    User component introduced

--- src/routes.ts
+++ src/routes.ts
@@ -1,12 +1,12 @@
 import React from "react";
 
 const Exchange = React.lazy(() => import('./components/exchange').then(x => ({ default: x.Exchange })))
-const Directory = React.lazy(() => import('./components/directory').then(x => ({ default: x.Directory })))
+const Directory = React.lazy(() => import('./components/user').then(x => ({ default: x.User })))
 const Settings = React.lazy(() => import('./components/settings').then(x => ({ default: x.Settings })))
 
 export const navItems: Link[] = [
     { name: 'Currency exchange', component: Exchange, to: '/', icon: 'monetization_on', exact: true, },
-    { name: 'Directory', component: Directory, to: `/directory`, icon: 'stars' },
+    { name: 'User', component: Directory, to: `/user`, icon: 'face' },
     { name: 'Settings', component: Settings, to: `/settings`, icon: 'build' },
 ];
 
