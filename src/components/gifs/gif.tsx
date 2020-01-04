c54e192ec06ad78776043802898d2d32782f24b6
Bartosz <bartosznovak@gmail.com>
2020-01-05 00:45:30 +0100

    Gif component restyled

--- src/components/gifs/gif.tsx
+++ src/components/gifs/gif.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Config } from 'react-promise-tracker';
+import { CircularProgress } from 'react-md/lib/Progress';
 import { GIFObject } from 'src/models/giphy';
 import { useGiphySearch } from 'src/models/giphy/hooks';
 import { useAsync } from 'src/utils/hooks';
-import { CircularProgress } from 'react-md/lib/Progress';
 
 const trackerConfig: Config = { area: 'gif-search', delay: 500 };
 type GifListProps = { keywords: string };
@@ -14,10 +14,17 @@ export const GifList: React.FunctionComponent<GifListProps> = ({ keywords }) => 
 
 const Gif: React.FunctionComponent<GIFObject> = gif => {
     const [loading, setLoading] = React.useState(true);
+    const [error, setError] = React.useState(false);
+    const onLoaded = React.useCallback(() => setLoading(false), [])
+    const onError = React.useCallback(() => {
+        setLoading(false);
+        setError(true)
+    }, [])
     return (
         <div style={styles.contentWrapper}>
             {loading && <CircularProgress id={gif.id} style={styles.spinner} />}
-            <video autoPlay loop src={gif.images.fixed_height.mp4} onLoadedData={() => setLoading(false)} />
+            {error && <div style={styles.error}>Preview unavailable</div>}
+            <video autoPlay loop src={gif.images.fixed_height.webp} onLoadedData={onLoaded} onError={onError} />
         </div>
     );
 };
@@ -29,13 +36,16 @@ const styles: GifsStyles = {
         justifyContent: 'center',
         alignItems: 'center',
         marginTop: 10,
-        marginRight: 10
+        marginRight: 10,
+        border: "1px solid #455A54"
     },
-    spinner: { position: 'absolute' }
+    spinner: { position: 'absolute' },
+    error: { position: 'absolute', color: '#90A4AE' },
 }
 
 type GifsStyles = {
     list: React.CSSProperties,
     contentWrapper: React.CSSProperties,
-    spinner: React.CSSProperties
+    spinner: React.CSSProperties,
+    error: React.CSSProperties
 }
